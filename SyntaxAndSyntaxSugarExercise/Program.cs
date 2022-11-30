fd98cdc4eb9511c90639f30b40932cc64d193486
Beau <jamesbuchanan0528@gmail.com>
2022-11-30 10:28:33 -0600

    SynSynSugEx

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace SyntaxAndSyntaxSugarExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿
+
+//Exercise 1:
+//Fork and Clone the repository to your local computer:
+//Syntax and Syntax Sugar Repo
+//Given the following code, change the code to implement:
+//inferred typing
+//string interpolation
+//and the ternary operator
+//int answer = 4;
+//string response;
+//if (answer < 9)
+//{
+//    response = answer + " is less than nine";
+//}
+//else
+//{
+//    response = answer + " is greater than or equal to nine";
+//}
+
+var answer = 4;
+var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than nine";
+Console.WriteLine(response);
+
